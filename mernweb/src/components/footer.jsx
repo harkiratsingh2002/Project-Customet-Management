61adda1a77e5357ef2f43ea5519cc406d799ebdf
Harkirat Singh <84682078+harkiratsingh2002@users.noreply.github.com>
2024-04-12 18:27:50 +0930

    Revert "update footer"
    This reverts commit 2f1437c46ffa5d7e51b7414101d705d0f907758d.

--- mernweb/src/components/footer.jsx
+++ mernweb/src/components/footer.jsx
@@ -4,120 +4,103 @@ import '../CSS/footer.css'
 export default function Footer() {
     return (
         // < !--Remove the container if you want to extend the Footer to full width. -- >
+        <div class="container my-5">
 
+            <footer class="text-white text-center text-lg-start" >
+                {/* <!-- Grid container --> */}
+                <div class="container p-4">
+                    {/* <!--Grid row--> */}
+                    <div class="row mt-4">
+                        {/* <!--Grid column--> */}
+                        <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+                            <h5 class="text-uppercase mb-4">About company</h5>
 
-        <footer class="text-white text-center text-lg-start" >
-            {/* <!-- Grid container --> */}
-            <div class="container p-4">
-                {/* <!--Grid row--> */}
-                <div class="row mt-4">
-                    {/* <!--Grid column--> */}
-                    <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
-                        <h5 class="text-uppercase mb-4">About company</h5>
+                            <p>
+                                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+                                voluptatum deleniti atque corrupti.
+                            </p>
 
-                        <p>
-                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-                            voluptatum deleniti atque corrupti.
-                        </p>
+                            <p>
+                                Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
+                                molestias.
+                            </p>
 
-                        <p>
-                            Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
-                            molestias.
-                        </p>
-
-                        <div class="mt-4 ">
-                            {/* <!-- Facebook --> */}
-                            <a type="button" class="btn btn-floating btn-warning btn-lg me-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
-                            </svg></a>
-                            {/* <!-- Linkedin --> */}
-                            <a type="button" class="btn btn-floating btn-warning btn-lg me-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-                            </svg></a>
-                            {/* <!-- Twitter --> */}
-                            <a type="button" class="btn btn-floating btn-warning btn-lg me-2 "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-                                <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-                            </svg></a>
-                            {/* <!-- Google + --> */}
-                            <a type="button" class="btn btn-floating btn-warning btn-lg me-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
-                                <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z" />
-                            </svg>   </a>
-
+                            <div class="mt-4">
+                                {/* <!-- Facebook --> */}
+                                <a type="button" class="btn btn-floating btn-warning btn-lg"><i class="fab fa-facebook-f"></i></a>
+                                {/* <!-- Dribbble --> */}
+                                <a type="button" class="btn btn-floating btn-warning btn-lg"><i class="fab fa-dribbble"></i></a>
+                                {/* <!-- Twitter --> */}
+                                <a type="button" class="btn btn-floating btn-warning btn-lg"><i class="fab fa-twitter"></i></a>
+                                {/* <!-- Google + --> */}
+                                <a type="button" class="btn btn-floating btn-warning btn-lg"><i class="fab fa-google-plus-g"></i></a>
+                                {/* <!-- Linkedin --> */}
+                            </div>
                         </div>
+                        {/* <!--Grid column--> */}
+
+                        {/* <!--Grid column--> */}
+                        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase mb-4 pb-1">Search something</h5>
+
+                            <div class="form-outline form-white mb-4">
+                                <input type="text" id="formControlLg" class="form-control form-control-lg" />
+                                <label class="form-label" for="formControlLg" >Search</label>
+                                <div class="form-notch"><div class="form-notch-leading" ></div><div class="form-notch-middle" ></div><div class="form-notch-trailing"></div></div></div>
+
+                            <ul class="fa-ul" >
+                                <li class="mb-3">
+                                    <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">New York, NY 10012, US</span>
+                                </li>
+                                <li class="mb-3">
+                                    <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">info@example.com</span>
+                                </li>
+                                <li class="mb-3">
+                                    <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+ 01 234 567 88</span>
+                                </li>
+                                <li class="mb-3">
+                                    <span class="fa-li"><i class="fas fa-print"></i></span><span class="ms-2">+ 01 234 567 89</span>
+                                </li>
+                            </ul>
+                        </div>
+                        {/* <!--Grid column--> */}
+
+                        {/* <!--Grid column--> */}
+                        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase mb-4">Opening hours</h5>
+
+                            <table class="table text-center text-white">
+                                <tbody class="font-weight-normal">
+                                    <tr>
+                                        <td>Mon - Thu:</td>
+                                        <td>8am - 9pm</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Fri - Sat:</td>
+                                        <td>8am - 1am</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Sunday:</td>
+                                        <td>9am - 10pm</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        {/* <!--Grid column--> */}
                     </div>
-                    {/* <!--Grid column--> */}
-
-                    {/* <!--Grid column--> */}
-                    <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-                        <h5 class="text-uppercase mb-4 pb-1">Search something</h5>
-
-                        <div class="form-outline form-white mb-4">
-                            <input type="text" id="formControlLg" class="form-control form-control-lg" />
-                            <label class="form-label" for="formControlLg" >Search</label>
-                            <div class="form-notch"><div class="form-notch-leading" ></div><div class="form-notch-middle" ></div><div class="form-notch-trailing"></div></div></div>
-
-                        <ul class="fa-ul" >
-                            <li class="mb-3">
-                                <span class="fa-li"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-                                    <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
-                                </svg></span><span class="ms-2">New York, NY 10012, US</span>
-                            </li>
-                            <li class="mb-3">
-                                <span class="fa-li"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
-                                </svg></span><span class="ms-2">info@example.com</span>
-                            </li>
-                            <li class="mb-3">
-                                <span class="fa-li"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
-                                    <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
-                                </svg></span><span class="ms-2">+ 01 234 567 88</span>
-                            </li>
-                            <li class="mb-3">
-                                <span class="fa-li"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
-                                    <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-                                    <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
-                                </svg></span><span class="ms-2">+ 01 234 567 89</span>
-                            </li>
-                        </ul>
-                    </div>
-                    {/* <!--Grid column--> */}
-
-                    {/* <!--Grid column--> */}
-                    <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-                        <h5 class="text-uppercase mb-4">Opening hours</h5>
-
-                        <table class="table text-center text-white">
-                            <tbody class="font-weight-normal">
-                                <tr>
-                                    <td>Mon - Thu:</td>
-                                    <td>8am - 9pm</td>
-                                </tr>
-                                <tr>
-                                    <td>Fri - Sat:</td>
-                                    <td>8am - 1am</td>
-                                </tr>
-                                <tr>
-                                    <td>Sunday:</td>
-                                    <td>9am - 10pm</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    {/* <!--Grid column--> */}
+                    {/* <!--Grid row--> */}
                 </div>
-                {/* <!--Grid row--> */}
-            </div>
-            {/* <!-- Grid container --> */}
+                {/* <!-- Grid container --> */}
 
-            {/* <!-- Copyright --> */}
-            <div class="text-center p-3" >
-                © 2020 Copyright:
-                <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-            </div>
-            {/* <!-- Copyright --> */}
-        </footer >
+                {/* <!-- Copyright --> */}
+                <div class="text-center p-3" >
+                    © 2020 Copyright:
+                    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+                </div>
+                {/* <!-- Copyright --> */}
+            </footer>
 
-
+        </div>
         /* <!-- End of .container --> */
     );
 }
