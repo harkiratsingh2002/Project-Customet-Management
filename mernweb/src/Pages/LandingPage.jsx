0a3be7a70ec43b7d5af97ba28d4cc2503eb52ae3
Harkirat Singh <84682078+harkiratsingh2002@users.noreply.github.com>
2024-04-12 12:07:14 +0930

    adding footer to landng page

--- mernweb/src/Pages/LandingPage.jsx
+++ mernweb/src/Pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Home from "../components/Home";
 import '../CSS/Navbar.css'
+import Footer from "../components/footer";
 
 export function Landing() {
     return (
         <div>
             <Navbar />
             <Home />
+            <Footer />
         </div>
     );
 }
