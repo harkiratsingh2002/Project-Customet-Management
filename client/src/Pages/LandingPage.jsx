df1f1acf942b0aa513235e8f2439242854c3dcee
Harkirat Singh <84682078+harkiratsingh2002@users.noreply.github.com>
2024-04-23 05:47:06 +0930

    home page update

--- client/src/Pages/LandingPage.jsx
+++ client/src/Pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import '../CSS/LandingPage.css'
 
 
 import Footer from "../components/footer";
-import bgImage1 from '../images/car-home-bg.jpg';
+import bgImage1 from '../images/car-home-bg.jpeg';
 
 export function Landing() {
     return (
@@ -11,19 +12,87 @@ export function Landing() {
         <><div> <Navbar /> </div>
             <div className="body">
                 <div>
-                    <section id="home" className="bg">
+                    <section id="home" className="bg ">
 
-                        <div class="card text-bg-light">
-                            <img src={bgImage1} class="img-fluid" alt="White Honda CRV car parked in a scenic location with white snow covered mountains in the background" />                                <div class="card-img-overlay">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                        <div class="card fluid text-bg-light ">
+                            <img src={bgImage1} class=" card-img img-fluid" alt="White car camped in an open field at night with an attached tent and fancy lights" />
+                            <div class="card-img-overlay text-center">
+                                <h1 class="card-title display-1 fw-bold text-color-light">Get your Dream Car Today!</h1>
+                                <h2 class="card-text  fw-bold text-color-light">Discover Top-Notch New and Pre-Owned Japanese Cars at Unbeatable Prices!</h2>
+
+
+                                <div className=" d-md-none">
+                                    <button>Search Cars</button>
+                                </div>
+
+                                <form action="#">
+                                    <div class="card rounded-pill  d-none d-md-block justify-content-center">
+                                        <div class="card-body">
+                                            <div class="row justify-content-center">
+                                                <div class="col-md">
+                                                    <div class="form-group ">
+                                                        <select id="inputState" class="form-control text-center border-top-0 border-bottom-0" >
+                                                            <option selected> New/Used</option>
+                                                            <option>New</option>
+                                                            <option>Used</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md">
+                                                    <div class="form-group ">
+                                                        <select id="inputState" class="form-control text-center border-top-0 border-bottom-0" >
+                                                            <option selected>Make</option>
+                                                            <option>BMW</option>
+                                                            <option>Audi</option>
+                                                            <option>Maruti</option>
+                                                            <option>Tesla</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md">
+                                                    <div class="form-group ">
+                                                        <select id="inputState" class="form-control text-center border-top-0 border-bottom-0" >
+                                                            <option selected>Model</option>
+                                                            <option>City</option>
+                                                            <option>Jazz</option>
+                                                            <option>Brio</option>
+
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md">
+                                                    <div class="form-group ">
+                                                        <select type="input" id="inputState" class="form-control text-center border-top-0 border-bottom-0" >
+                                                            <option selected>Price</option>
+                                                            <option>$5,000-$10,000</option>
+                                                            <option>$10,000-$20,000</option>
+                                                            <option>$20,000-$40,000</option>
+                                                            <option>$40,000-$60,000</option>
+                                                            <option>$60,000-$100,000</option>
+                                                            <option>$100,000 and Above </option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-auto">
+                                                    <button type="submit" class="btn  btn-success dark-bg text-light">Search</button>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                                <h5 class="card-text fst-italic text-color-light">See our <a href="#/vehicles" class="text-color-light">car collection</a> to view all</h5>
                             </div>
                         </div>
-                    </section>
 
-                </div>
-            </div>
+
+                    </section>
+                </div >
+            </div >
+
+
+
             <div><Footer /></div></>
     );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,28 +17,28 @@ const Navbar = () => {
                 <div class="collapse navbar-collapse fw-bold " id="navbarNavDropdown">
                     <ul class="navbar-nav col-8">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active dark-text" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Dashoard</a>
+                            <a class="nav-link dark-text" href="#">Dashoard</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">All Vehicles</a>
+                            <a class="nav-link dark-text" href="#">All Vehicles</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle dark-text" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 About Us
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#/contact">Contact Us</a></li>
-                                <li><a class="dropdown-item" href="#">Our Story</a></li>
+                                <li><a class="dropdown-item dark-text" href="#/contact">Contact Us</a></li>
+                                <li><a class="dropdown-item dark-text" href="#">Our Story</a></li>
 
                             </ul>
                         </li>
                     </ul>
                     <div class="col">
-                        <a href="#/login"><button type="button" class="btn btn-dark me-3  ">Log In</button></a>
-                        <a href="#/register"><button type="button" class="btn btn-dark me-3 ">Sign In</button></a>
+                        <a href="#/login"><button type="button" class="btn dark-bg light-text me-3  ">Log In</button></a>
+                        <a href="#/register"><button type="button" class="btn dark-bg light-text me-3 ">Sign In</button></a>
                     </div>
                 </div>
 
