95272b0e83bf1fc3dca4cc18b9a2e2bc224cd085
Harkirat Singh <84682078+harkiratsingh2002@users.noreply.github.com>
2024-04-22 16:13:51 +0930

    navbar update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import '../CSS/Navbar.css'
+import logo from '../images/logo.png';
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light shadow ">
-                <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light shadow pt-0 pb-0 navbar-custom sticky-top ">
+                <div class="container-fluid  pe-0 ps-0">
 
 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div className="navbar-brand me-0">
+                            <a href="#" class="navbar-brand"><img src={logo} alt="" width={120} height={110} /></a>
+                        </div>
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -27,12 +31,9 @@ const Navbar = () => {
 
 
                         </ul>
-                        <a class="navbar-brand fw-bolder fs-4 mx-auto " href="#">Company name</a>
-                        <a href="#/login"><button className="btn btn-outline-primary ms-auto px-4 rounded-pill LogIn">
 
-                            <i className="fa fa-sign-in me-2"></i>Login</button></a>
-                        <a href="#/register"><button className="btn btn-outline-primary ms-2 px-4 rounded-pill LogIn">
-                            <i className="fa fa-user-plus me-2"></i>Register</button></a>
+                        <a class="me-3" href="#/login"><button type="button" class="btn btn-outline-secondary">Log In</button></a>
+                        <a class="me-3" href="#/register"><button type="button" class="btn btn-outline-secondary">Register</button></a>
 
                     </div>
                 </div>
